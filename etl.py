3e21050dc833a08a947dbf4e9e2bbb0008195e7f
maciejziomber <ziombermaciej@gmail.com>
2026-02-12 14:56:22 -0700

    Modified the code

--- etl.py
+++ etl.py
@@ -5,7 +5,7 @@ import json
 df = pd.read_csv('data/raw_data.csv')
 
 #Transform
-df['city'] = df['city'].str.upper()
+df['city '] = df['city '].str.upper()
 
 #Load (save as Json)
 df.to_json('data/processed_data.json', orient='records', indent=4)
