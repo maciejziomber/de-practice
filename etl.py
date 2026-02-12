e5f6b311ac2b93671bde13808cdee6954043e8b0
maciejziomber <ziombermaciej@gmail.com>
2026-02-12 14:23:49 -0700

    Initial commit: Added ETL script and raw data

---
+++ etl.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import json
+
+#Extract
+df = pd.read_csv('data/raw_data.csv')
+
+#Transform
+df['city'] = df['city'].str.upper()
+
+#Load (save as Json)
+df.to_json('data/processed_data.json', orient='records', indent=4)
+print("Data Engineering pipeline complete!")
